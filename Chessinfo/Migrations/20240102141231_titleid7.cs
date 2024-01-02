1564dc9a7ad6ae54631855675e99788f12d7d5b4
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2024-01-02 15:19:07 +0100

    removed title column from player, replaced with titleId

---
+++ Chessinfo/Migrations/20240102141231_titleid7.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chessinfo.Migrations
+{
+    /// <inheritdoc />
+    public partial class titleid7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Chessinfo/Migrations/20240102141611_titleid8.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chessinfo.Migrations
+{
+    /// <inheritdoc />
+    public partial class titleid8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
